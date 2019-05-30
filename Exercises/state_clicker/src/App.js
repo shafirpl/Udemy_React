2de69fee8d69610af84a8766561cc5475dbac388
shafirpl <shafi.rpl@gmail.com>
2019-05-29 22:48:13 -0700

    Props Basics done

---
+++ Exercises/state_clicker/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import Clicker from "./Clicker";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Clicker />
+    </div>
+  );
+}
+
+export default App;
