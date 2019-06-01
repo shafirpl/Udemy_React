fe9f6bfb52f4f28f547b3e71dee844e25064143e
Shafi Hoque <shafihoque@Shafis-MacBook-Air.local>
2019-06-01 04:06:16 -0700

    Dice Exercise

---
+++ Projects/dice_project/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import RollDie from './RollDice.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <RollDie />
+    </div>
+  );
+}
+
+export default App;
