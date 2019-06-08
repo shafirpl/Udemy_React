4da079955459487b0062e273b7d850770e0257f9
Shafi Hoque <shafihoque@Shafis-MacBook-Air.local>
2019-06-07 18:27:05 -0700

    Hangman start

--- Projects/react-hangman-starter/src/Hangman.js
+++ Projects/react-hangman-starter/src/Hangman.js
@@ -24,6 +24,17 @@ class Hangman extends Component {
   /** guessedWord: show current-state of word:
     if guessed letters are {a,p,e}, show "app_e" for "apple"
   */
+
+  /*
+   * https://www.udemy.com/modern-react-bootcamp/learn/lecture/14375668#questions
+   * Watch from 4:07 to 6:21
+   * So basically it is taking the string, making it an array
+   * or transforming the string to an array
+   * so "Apple" will become ["A","p","p","l","e"], and afterwards
+   * with .split(""), and then running a loop for each element and
+   * checking to see if that word exists in the guessed array
+   * that checking is being done by
+   */
   guessedWord() {
     return this.state.answer
       .split("")
@@ -34,6 +45,10 @@ class Hangman extends Component {
     - add to guessed letters
     - if not in answer, increase number-wrong guesses
   */
+  /*
+   * https://www.udemy.com/modern-react-bootcamp/learn/lecture/14375668#questions
+   * Watch from 8:02 to 
+   */
   handleGuess(evt) {
     let ltr = evt.target.value;
     this.setState(st => ({
@@ -43,9 +58,21 @@ class Hangman extends Component {
   }
 
   /** generateButtons: return array of letter buttons to render */
+  /*
+   * Similarly, we have a-z, and then we are transfroming the whole
+   * string to an array, and then running a loop and for every
+   * element in that array, we are creating a button with the value,
+   * adding an event listener and adding disabled property
+   */
   generateButtons() {
     return "abcdefghijklmnopqrstuvwxyz".split("").map(ltr => (
       <button
+        // adding key here
+        //Key has to be unique, however since 
+        //each letter is being used, and not duplicated
+        //letters are used, it is better just to use
+        //the letter itself
+        key = {ltr}
         value={ltr}
         onClick={this.handleGuess}
         disabled={this.state.guessed.has(ltr)}
@@ -58,11 +85,11 @@ class Hangman extends Component {
   /** render: render game */
   render() {
     return (
-      <div className='Hangman'>
+      <div className="Hangman">
         <h1>Hangman</h1>
         <img src={this.props.images[this.state.nWrong]} />
-        <p className='Hangman-word'>{this.guessedWord()}</p>
-        <p className='Hangman-btns'>{this.generateButtons()}</p>
+        <p className="Hangman-word">{this.guessedWord()}</p>
+        <p className="Hangman-btns">{this.generateButtons()}</p>
       </div>
     );
   }
