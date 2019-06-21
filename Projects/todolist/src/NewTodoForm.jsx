a6ef08ccd7944559c6cf11c1c38e3f31fe611faf
Shafi Hoque <shafihoque@Shafis-MacBook-Air.local>
2019-06-20 21:11:33 -0700

    Section 15 start

---
+++ Projects/todolist/src/NewTodoForm.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import uuid from 'uuid/v4';
+
+
+class NewTodoForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { task: "" };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(evt){
+        this.setState({
+            [evt.target.name]: evt.target.value
+        })
+    }
+
+    handleSubmit(evt){
+        evt.preventDefault();
+        const newTodo = {...this.state, id:uuid()};
+        this.props.createTodo(newTodo);
+        this.setState({
+            task:""
+        })
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label htmlFor="task">New Todo</label>
+                <input
+                    id="task"
+                    name = "task"
+                    type="text"
+                    placeholder="New Todo"
+                    value={this.state.task}
+                    onChange = {this.handleChange}
+                />
+                <button>Add Todo</button>
+            </form>
+        );
+    }
+}
+
+export default NewTodoForm;
