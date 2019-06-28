17fef6502ff734a31fd391796a2242ad25191243
shafirpl <shafi.rpl@gmail.com>
2019-06-28 09:57:33 -0700

    Yahtzee start

---
+++ Projects/yahtzee-starter/src/Game.js
@@ -0,0 +1,104 @@
+import React, { Component } from "react";
+import Dice from "./Dice";
+import ScoreTable from "./ScoreTable";
+import "./Game.css";
+
+const NUM_DICE = 5;
+const NUM_ROLLS = 3;
+
+class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //creating a dice array that has NUM_DICE length
+      // usually Array.from is used to copy an array
+      // but this is a nice shortcut to create an
+      // array of length something in js
+      dice: Array.from({ length: NUM_DICE }),
+      // Search array.fill method, but what it does
+      // is that it is creating an array of NUM_DICE elements
+      // and then filling all the values with false
+      locked: Array(NUM_DICE).fill(false),
+      rollsLeft: NUM_ROLLS,
+      scores: {
+        ones: undefined,
+        twos: undefined,
+        threes: undefined,
+        fours: undefined,
+        fives: undefined,
+        sixes: undefined,
+        threeOfKind: undefined,
+        fourOfKind: undefined,
+        fullHouse: undefined,
+        smallStraight: undefined,
+        largeStraight: undefined,
+        yahtzee: undefined,
+        chance: undefined
+      }
+    };
+    this.roll = this.roll.bind(this);
+    this.doScore = this.doScore.bind(this);
+  }
+
+  roll(evt) {
+    // roll dice whose indexes are in reroll
+    this.setState(st => ({
+      dice: st.dice.map((d, i) =>
+        st.locked[i] ? d : Math.ceil(Math.random() * 6)
+      ),
+      locked: st.rollsLeft > 1 ? st.locked : Array(NUM_DICE).fill(true),
+      rollsLeft: st.rollsLeft - 1
+    }));
+  }
+
+  toggleLocked(idx) {
+    // toggle whether idx is in locked or not
+    this.setState(st => ({
+      locked: [
+        ...st.locked.slice(0, idx),
+        !st.locked[idx],
+        ...st.locked.slice(idx + 1)
+      ]
+    }));
+  }
+
+  doScore(rulename, ruleFn) {
+    // evaluate this ruleFn with the dice and score this rulename
+    this.setState(st => ({
+      scores: { ...st.scores, [rulename]: ruleFn(this.state.dice) },
+      rollsLeft: NUM_ROLLS,
+      locked: Array(NUM_DICE).fill(false)
+    }));
+    this.roll();
+  }
+
+  render() {
+    return (
+      <div className='Game'>
+        <header className='Game-header'>
+          <h1 className='App-title'>Yahtzee!</h1>
+
+          <section className='Game-dice-section'>
+            <Dice
+              dice={this.state.dice}
+              locked={this.state.locked}
+              handleClick={this.toggleLocked}
+            />
+            <div className='Game-button-wrapper'>
+              <button
+                className='Game-reroll'
+                disabled={this.state.locked.every(x => x)}
+                onClick={this.roll}
+              >
+                {this.state.rollsLeft} Rerolls Left
+              </button>
+            </div>
+          </section>
+        </header>
+        <ScoreTable doScore={this.doScore} scores={this.state.scores} />
+      </div>
+    );
+  }
+}
+
+export default Game;
