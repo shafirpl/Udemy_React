242f2f0d6c06579509e4f5877756e6689156eda3
shafirpl <shafi.rpl@gmail.com>
2019-07-07 12:14:36 -0700

    Working on Vending machine

---
+++ Projects/vending-machine/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+import Chips from './Chips';
+import Soda from './Soda';
+import Sardines from './Sardines';
+import VendingMachine from './VendingMachine';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" render={() => <VendingMachine />} />
+        <Route exact path="/soda" render={() => <Soda />} />
+        <Route exact path="/sardines" render={() => <Sardines />} />
+        <Route exact path="/chips" render={() => <Chips />} />
+      </Switch>
+      
+    </div>
+  );
+}
+
+export default App;
