07d7696c8e6fafa00b0fedfca9773e13cd4c620c
shafirpl <shafi.rpl@gmail.com>
2019-07-10 04:14:33 -0700

    Working on color app

--- Projects/colors-app/src/ColorBox.jsx
+++ Projects/colors-app/src/ColorBox.jsx
@@ -1,14 +1,23 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import './ColorBox.css';
 
- class ColorBox extends Component {
-     render() {
-         return (
-             <div style = {{background: this.props.background}} className='ColorBox'>
-                 <span>{this.props.name}</span>
-                 <span>More</span>
-             </div>
-         );
-     }
- }
+class ColorBox extends Component {
 
- export default ColorBox;
+    render() {
+        const { name, background } = this.props;
+        return (
+            //this is equivalent to style = {{background: background}}
+            <div className='ColorBox' style={{ background }} >
+                <div className="copy-container">
+                    <div className="box-content">
+                        <span>{name}</span>
+                    </div>
+                    <button className="copy-button">Copy</button>
+                </div>
+                <span className="see-more">More</span>
+            </div>
+        );
+    }
+}
+
+export default ColorBox;
--- Projects/colors-app/src/Palette.jsx
+++ Projects/colors-app/src/Palette.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import ColorBox from './ColorBox.jsx';
+import './Palette.css';
 
 class Palette extends Component {
     render() { 
