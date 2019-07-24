005d715b309d4bc94e694a704a89dbdb37719cd0
Shafi Hoque <shafihoque@Shafis-MacBook-Air.local>
2019-07-23 20:55:11 -0700

    Working on part 2 of color app

--- ColorAppProject/colors-app/src/Navbar.jsx
+++ ColorAppProject/colors-app/src/Navbar.jsx
@@ -2,10 +2,32 @@ import React, { Component } from "react";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import "./Navbar.css";
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import Snackbar from '@material-ui/core/Snackbar';
+import CloseIcon from '@material-ui/icons/Close';
+
 
 export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { format: "hex" };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(evt) {
+    /*
+    * the props.handleChange corresponds to the changeFormat function
+    * in Paletter.jsx file. Passing a value to that function will change
+    * the format of color in Paletter.jsx state when the function is triggered.
+    */
+    this.setState({ format: evt.target.value });
+    this.props.handleChange(evt.target.value);
+    ;
+  }
   render() {
     const { level, changeLevel } = this.props;
+    const { format } = this.state;
     return (
       <header className="Navbar">
         <div className="logo">
@@ -35,6 +57,28 @@ export default class Navbar extends Component {
             />
           </div>
         </div>
+        <div className="select-container">
+          {/* 
+          * this select component from material ui
+          * will help us to choose format. For example:
+          * if we chose hex, then clicking any color will copy its hex
+          * value, chosing rgb will copy its rgb value and so on.
+          * Remember, if we don't pass parameters/arguments to a function that
+          * is triggered when something changes (onChange property), it will take the
+          * whole event object as the parameter/argument. Then we do evt.target.value 
+          * to grab the values.
+          */}
+          <Select
+            value={format}
+            onChange={this.handleChange}>
+            <MenuItem value="hex"> HEX - #fff</MenuItem>
+            <MenuItem value="rgb"> RGB - rgb(255,255,255)</MenuItem>
+            <MenuItem value="rgba"> RGBA - rgba(255,255,255,1.0)</MenuItem>
+          </Select>
+        </div>
+        <Snackbar anchorOrigin>
+
+        </Snackbar>
       </header>
     );
   }
--- ColorAppProject/colors-app/src/Palette.jsx
+++ ColorAppProject/colors-app/src/Palette.jsx
@@ -6,14 +6,18 @@ import "./Palette.css";
 class Palette extends Component {
   constructor(props) {
     super(props);
-    this.state = { level: 500 };
+    this.state = { level: 500, format: "hex" };
     this.changeLevel = this.changeLevel.bind(this);
+    this.changeFormat = this.changeFormat.bind(this);
   }
 
   changeLevel(newLevel) {
     this.setState({ level: newLevel });
   }
 
+  changeFormat(val){
+    this.setState({format: val});
+  }
   render() {
     /*
      * This is for my own reference, if we
@@ -31,9 +35,9 @@ class Palette extends Component {
     // );
 
     const { colors } = this.props.palette;
-    const { level } = this.state;
+    const { level, format } = this.state;
     const colorBoxes = colors[level].map(color => (
-      <ColorBox background={color.hex} name={color.name} />
+      <ColorBox background={color[format]} name={color.name} />
     ));
     return (
       <div className="Palette">
@@ -41,7 +45,8 @@ class Palette extends Component {
 
         <Navbar 
             level={level}
-            changeLevel = {this.changeLevel}/>
+            changeLevel = {this.changeLevel}
+            handleChange = {this.changeFormat}/>
 
         <div className="Palette-colors">
           {/* bunch of color boxes */}
